3ed023bf64a959118096a384f038fadeeb3e84a6
SUN <sunxiang0918@gmail.com>
2015-07-18 16:47:29 +0800

    完成点赞的组件的东西. 现在还差的就是  带箭头的 Pop框了

--- ClickZanButton-Swift/ViewController.swift
+++ ClickZanButton-Swift/ViewController.swift
@@ -1,25 +1,53 @@
 //
 //  ViewController.swift
-//  ClickZanButton-Swift
+//  ClickZhanButton
 //
-//  Created by SUN on 15/7/18.
+//  Created by SUN on 15/7/14.
 //  Copyright © 2015年 SUN. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var zanView: ZanButton!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var numberScrollView: NumberScrollAnimatedView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        zanView.zanAction = {(zhanNumber)->Void in
+            self.label.text = "\(zhanNumber)"
+            self.label.textColor = UIColor(red: 0.098, green: 0.565, blue: 0.827, alpha: 1)
+        }
+        
+        zanView.unzanAction = {(zhanNumber)->Void in
+            self.label.text = "\(zhanNumber)"
+            self.label.textColor = UIColor.blackColor()
+        }
+        
+        numberScrollView.value = 0
+        numberScrollView.desity = 5
+        numberScrollView.duration = 1.5
+        numberScrollView.durationOffset = 0.2
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func doAction(sender: UIButton) {
+        numberScrollView.value = random()
+        numberScrollView.startAnimation()
+        
+    }
+    
 }
 
---
+++ ClickZanButton-Swift/ZanButton.swift
@@ -0,0 +1,160 @@
+//
+//  ZanButton.swift
+//  ClickZanButton
+//
+//  Created by SUN on 15/7/14.
+//  Copyright © 2015年 SUN. All rights reserved.
+//
+
+import UIKit
+
+class ZanButton: UIView{
+
+    private var zanImageView:UIImageView!
+    
+    private var zanNotifyView:UIView!
+    
+    private var numberView:NumberScrollAnimatedView!
+    
+    var zanAction:((Int)->Void)?       //点赞的时候的动作
+    var unzanAction:((Int)->Void)?     //点取消赞的时候的动作
+    
+    var zanImage:UIImage! {
+        didSet{
+            if !isZan {
+                zanImageView.image = zanImage
+            }
+        }
+    }
+    var zanedImage:UIImage! {
+        didSet{
+            if  isZan {
+                zanImageView.image = zanedImage
+            }
+        }
+    }
+    
+    var isZan = false
+    
+    var zanNumber = 0      //赞的数量
+    
+    var popHeight:Float = 20.0
+    var popBackgroundColor = UIColor(red: 0.098, green: 0.565, blue: 0.827, alpha: 1)
+    var popCornerRadius:Float = 2.0
+    var popFont = UIFont.systemFontOfSize(10)
+    var popFontColor = UIColor.whiteColor()
+    var popDuration:Float = 0.4
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initBaseLayout()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initBaseLayout()
+    }
+    
+    private func initBaseLayout(){
+        
+        //设置图片
+        zanImageView = UIImageView(frame: CGRectMake(0, 0, CGRectGetWidth(self.frame), CGRectGetHeight(self.frame)))
+        zanImageView.contentMode = UIViewContentMode.Center
+        
+        zanImageView.userInteractionEnabled = true
+        self.addSubview(zanImageView)
+        
+        if zanImage == nil {
+            zanImage = UIImage(named: "News_Navigation_Vote")
+        }
+        
+        if zanedImage == nil {
+            zanedImage = UIImage(named: "News_Navigation_Voted")
+        }
+        
+        //设置点击事件
+        let tapImageViewGesture = UITapGestureRecognizer(target: self, action: "zanAnimationPlay")
+        
+        zanImageView.addGestureRecognizer(tapImageViewGesture)
+        
+        //开始构建
+        zanNotifyView = UIView(frame: CGRectMake(0, 0, CGRectGetWidth(self.frame), CGFloat(popHeight)))
+        zanNotifyView.backgroundColor=popBackgroundColor
+        
+        //设置圆角矩形
+        zanNotifyView.layer.cornerRadius = CGFloat(popCornerRadius);
+        zanNotifyView.layer.masksToBounds = true;
+        
+        //先是设置为隐藏
+        zanNotifyView.alpha=0
+        
+        //开始设置数字了
+        numberView = NumberScrollAnimatedView(frame: CGRectMake(0, 0, CGRectGetWidth(self.frame), CGFloat(popHeight)))
+        numberView.backgroundColor = UIColor.clearColor()
+        numberView.font = popFont
+        numberView.textColor = popFontColor
+        numberView.desity = 0
+        numberView.duration = popDuration
+        numberView.value = zanNumber
+        
+        zanNotifyView.addSubview(numberView!)
+        
+        self.addSubview(zanNotifyView)
+    }
+    
+    func zanAnimationPlay(){
+        
+        //点击取反
+        self.isZan = !self.isZan
+        
+        if self.isZan {
+            //赞的数量加1
+            self.zanNumber++
+            
+            //开始执行动画
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                //第一阶段动画效果,用于显示popView
+                self.zanNotifyView?.frame = CGRectMake(0, -30, CGRectGetWidth(self.frame), CGFloat(self.popHeight))
+                self.zanNotifyView?.alpha = 1
+                }, completion: { (finished) -> Void in
+                    //完成第一阶段动画后, 开始跳动数字
+                    
+                    self.numberView.value = self.zanNumber
+                    self.numberView.startAnimation({()->Void in
+                        //数字跳动完成后,开始 第二阶段动画, 用于popView的消失
+                        UIView.animateWithDuration(0.3, animations: { () -> Void in
+                            //动画效果
+                            self.zanNotifyView?.frame = CGRectMake(0, 0, CGRectGetWidth(self.frame), CGFloat(self.popHeight))
+                            self.zanNotifyView?.alpha = 0
+                            
+                            }, completion: { (finished) -> Void in
+                                //完成
+                                
+                        })
+                    })
+            })
+            
+            if let _zanAction = zanAction {
+                _zanAction(self.zanNumber)
+            }
+            self.zanImageView?.image = zanedImage
+        }else{
+            //如果是取消赞的操作
+            self.zanNumber--
+            self.numberView.value = self.zanNumber
+            self.zanImageView?.image = zanImage
+            
+            if  let _unzanAction = unzanAction {
+                _unzanAction(self.zanNumber)
+            }
+        }
+    }
+}
